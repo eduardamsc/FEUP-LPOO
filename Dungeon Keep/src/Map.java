20b23e2121fa2f75f00f89f518819d9b1ad4e691
eduardamsc <mariaeduardascunha97@gmail.com>
2017-02-13 17:11:47 +0000

    SIMPLIFICADO

--- Dungeon Keep/src/Map.java
+++ Dungeon Keep/src/Map.java
@@ -2,68 +2,16 @@
 public class Map {
 
 	public static void main(String[] args) {
-		char[][] map = new char[10][10];
-
-		//inicializar toda a matriz com espaços
-		for (int i = 0; i < map.length; i++) {
-			for (int j = 0; j < map[i].length; j++) {
-				map[i][j] = ' ';
-			}
-		}
-
-		// 1ºlinha com x
-		for (int i = 0; i < map[0].length; i++) {
-			map[0][i] = 'X';
-		}
-
-		// ultima linha com x
-		for (int i = 0; i < map[9].length; i++) {
-			map[9][i] = 'X';
-		}
-
-		// ultima lateral com x
-		for (int i = 0; i < map.length; i++) {
-			map[i][9] = 'X';
-		}
-
-		for (int i = 0; i < map.length; i++) {
-			if (i == 5 || i == 6) {
-				map[i][0] = 'I';
-			} else {
-				map[i][0] = 'X';
-			}
-		}
-
-		map[1][6] = 'X';
-		map[2][1] = 'X';
-		map[2][2] = 'X';
-		map[2][4] = 'X';
-		map[2][5] = 'X';
-		map[2][6] = 'X';
-		map[3][6] = 'X';
-		map[4][1] = 'X';
-		map[4][2] = 'X';
-		map[4][4] = 'X';
-		map[4][5] = 'X';
-		map[4][6] = 'X';
-		map[7][1] = 'X';
-		map[7][2] = 'X';
-		map[7][4] = 'X';
-		map[7][5] = 'X';
-		map[7][6] = 'X';
-		map[7][7] = 'X';
-		map[8][6] = 'X';
-		
-		
-		map[1][4] = 'I';
-		map[3][4] = 'I';
-		map[3][2] = 'I';
-		map[8][2] = 'I';
-		map[8][4] = 'I';
-
-		map[1][1] = 'H';
-		map[1][8] = 'G';
-		map[8][7] = 'K';
+		char[][] map = { { 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' },
+				{ 'X', 'H', ' ', ' ', 'I', ' ', 'X', ' ', 'G', 'X' },
+				{ 'X', 'X', 'X', ' ', 'X', 'X', 'X', ' ', ' ', 'X' },
+				{ 'X', ' ', 'I', ' ', 'I', ' ', 'X', ' ', ' ', 'X' },
+				{ 'X', 'X', 'X', ' ', 'X', 'X', 'X', ' ', ' ', 'X' },
+				{ 'I', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+				{ 'I', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+				{ 'X', 'X', 'X', ' ', 'X', 'X', 'X', 'X', ' ', 'X' },
+				{ 'X', ' ', 'I', ' ', 'I', ' ', 'X', 'K', ' ', 'X' },
+				{ 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' } };
 
 		for (int i = 0; i < map.length; i++) {
 			for (int j = 0; j < map[i].length; j++) {
@@ -71,5 +19,6 @@ public class Map {
 			}
 			System.out.println();
 		}
+		 
 	}
 }
