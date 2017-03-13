577ad3a359327d5ce0cabe64d52d42de6fa48428
eduardamsc <mariaeduardascunha97@gmail.com>
2017-03-13 16:13:56 +0000

    iteraçao 4 começada

---
+++ Dungeon Keep/src/dkeep/gui/window1.java
@@ -0,0 +1,106 @@
+package dkeep.gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import java.awt.Font;
+
+public class window1 {
+
+	private JFrame frame;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					window1 window = new window1();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public window1() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 536, 388);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNumberOfOgres = new JLabel("Number of Ogres");
+		lblNumberOfOgres.setBounds(20, 31, 113, 16);
+		frame.getContentPane().add(lblNumberOfOgres);
+		
+		JLabel lblGuardPersonality = new JLabel("Guard personality");
+		lblGuardPersonality.setBounds(20, 59, 141, 16);
+		frame.getContentPane().add(lblGuardPersonality);
+		
+		textField = new JTextField();
+		textField.setBounds(150, 28, 43, 21);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(150, 55, 127, 27);
+		frame.getContentPane().add(comboBox);
+		
+		JButton btnNewGame = new JButton("New Game");
+		btnNewGame.setBounds(393, 91, 117, 29);
+		frame.getContentPane().add(btnNewGame);
+		
+		JButton btnExit = new JButton("Exit");
+		btnExit.setBounds(393, 301, 117, 29);
+		frame.getContentPane().add(btnExit);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setFont(new Font("Courier New", Font.PLAIN, 13));
+		textArea.setBounds(20, 98, 329, 221);
+		frame.getContentPane().add(textArea);
+		
+		JButton btnUp = new JButton("Up");
+		btnUp.setEnabled(false);
+		btnUp.setBounds(406, 153, 78, 29);
+		frame.getContentPane().add(btnUp);
+		
+		JButton btnLeft = new JButton("Left");
+		btnLeft.setEnabled(false);
+		btnLeft.setBounds(360, 182, 79, 29);
+		frame.getContentPane().add(btnLeft);
+		
+		JButton btnRight = new JButton("Right");
+		btnRight.setEnabled(false);
+		btnRight.setBounds(451, 182, 79, 29);
+		frame.getContentPane().add(btnRight);
+		
+		JButton btnDown = new JButton("Down");
+		btnDown.setEnabled(false);
+		btnDown.setBounds(405, 212, 79, 29);
+		frame.getContentPane().add(btnDown);
+		
+		JLabel lblGameStatus = new JLabel("Game status");
+		lblGameStatus.setBounds(20, 327, 252, 16);
+		frame.getContentPane().add(lblGameStatus);
+	}
+}
