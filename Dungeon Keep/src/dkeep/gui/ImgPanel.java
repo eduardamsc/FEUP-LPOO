9e954143aad18cb3c4f638d778886a779786a348
eduardamsc <mariaeduardascunha97@gmail.com>
2017-03-24 01:36:31 +0000

    interface em progresso

--- Dungeon Keep/src/dkeep/gui/ImgPanel.java
+++ Dungeon Keep/src/dkeep/gui/ImgPanel.java
@@ -11,11 +11,19 @@ import javax.swing.JPanel;
 public class ImgPanel extends JPanel {
 	private BufferedImage start;
 	private BufferedImage wall;
+	private BufferedImage exitClosed;
+	private BufferedImage exitOpened;
 	private BufferedImage hero;
 	private BufferedImage guard;
+	private BufferedImage ogre;
+	private BufferedImage leverClosed;
+	private BufferedImage leverOpened;
+	private BufferedImage key;
+	
 	private char[][] map = new char[][] { {'D'}};
 	
 	public ImgPanel(){
+
 		try {
 			this.start = ImageIO.read( new File( "/Users/eduardacunha/Desktop/Picture1.png" ) );
 			
@@ -23,6 +31,7 @@ public class ImgPanel extends JPanel {
 			System.out.println(e1.getCause());
 			e1.printStackTrace();
 		}	
+////////////////////////////////////////MAP/////////////////////////////////////////////////
 		try {
 			this.wall = ImageIO.read( new File( "/Users/eduardacunha/Documents/Uni/2º/LPOO/images/wall.png" ) );
 			
@@ -30,8 +39,24 @@ public class ImgPanel extends JPanel {
 			System.out.println(e1.getCause());
 			e1.printStackTrace();
 		}
+		
 		try {
-			this.hero = ImageIO.read( new File( "/Users/eduardacunha/Documents/Uni/2º/LPOO/images/12OgreTras.png" ) );
+			this.exitClosed = ImageIO.read( new File( "/Users/eduardacunha/Documents/Uni/2º/LPOO/images/exitClosed.png" ) );
+			
+		} catch (IOException e1) {
+			System.out.println(e1.getCause());
+			e1.printStackTrace();
+		}
+		try {
+			this.exitOpened = ImageIO.read( new File( "/Users/eduardacunha/Documents/Uni/2º/LPOO/images/exitOpened.png" ) );
+			
+		} catch (IOException e1) {
+			System.out.println(e1.getCause());
+			e1.printStackTrace();
+		}
+////////////////////////////////////////CHARACTERS/////////////////////////////////////////////////
+		try {
+			this.hero = ImageIO.read( new File( "/Users/eduardacunha/Documents/Uni/2º/LPOO/images/ogreFront.png" ) );
 			
 		} catch (IOException e1) {
 			System.out.println(e1.getCause());
@@ -43,7 +68,36 @@ public class ImgPanel extends JPanel {
 		} catch (IOException e1) {
 			System.out.println(e1.getCause());
 			e1.printStackTrace();
-		}	
+		}
+		try {
+			this.ogre = ImageIO.read( new File( "/Users/eduardacunha/Documents/Uni/2º/LPOO/images/OgreFront.png" ) );
+			
+		} catch (IOException e1) {
+			System.out.println(e1.getCause());
+			e1.printStackTrace();
+		}
+////////////////////////////////////////OBJECTS/////////////////////////////////////////////////
+		try {
+			this.leverClosed = ImageIO.read( new File( "/Users/eduardacunha/Documents/Uni/2º/LPOO/images/leverClosed.png" ) );
+			
+		} catch (IOException e1) {
+			System.out.println(e1.getCause());
+			e1.printStackTrace();
+		}
+		/*try {
+			this.leverOpened = ImageIO.read( new File( "/Users/eduardacunha/Documents/Uni/2º/LPOO/images/leverOpened.png" ) );
+			
+		} catch (IOException e1) {
+			System.out.println(e1.getCause());
+			e1.printStackTrace();
+		}*/
+		try {
+			this.key = ImageIO.read( new File( "/Users/eduardacunha/Documents/Uni/2º/LPOO/images/key.png" ) );
+			
+		} catch (IOException e1) {
+			System.out.println(e1.getCause());
+			e1.printStackTrace();
+		}
 	}
 	
 	public void updateMap(char[][] map)
@@ -61,10 +115,20 @@ public class ImgPanel extends JPanel {
 				{
 					g.drawImage(this.start, this.start.getWidth() * i, this.start.getWidth() * j, null);
 				}
+////////////////////////////////////////MAP/////////////////////////////////////////////////
 				if(this.map[j][i]=='X')
 				{
 					g.drawImage(this.wall, this.wall.getWidth() * i, this.wall.getHeight() * j, null);
 				}
+				if(this.map[j][i]=='I')
+				{
+					g.drawImage(this.exitClosed,this.wall.getWidth() * i, this.wall.getHeight() * j,null);
+				}
+				if(this.map[j][i]=='S')
+				{
+					g.drawImage(this.exitOpened, this.wall.getWidth() * i, this.wall.getHeight() * j, null);
+				}
+////////////////////////////////////////CHARACTERS/////////////////////////////////////////////////
 				if(this.map[j][i]=='H')
 				{
 					g.drawImage(this.hero, this.wall.getWidth() * i, this.wall.getHeight() * j, null);
@@ -73,6 +137,20 @@ public class ImgPanel extends JPanel {
 				if(this.map[j][i]=='G')
 				{
 					g.drawImage(this.guard, this.wall.getWidth() * i, this.wall.getHeight() * j, null);
+				}
+				
+				if(this.map[j][i]=='O')
+				{
+					g.drawImage(this.ogre, this.wall.getWidth() * i, this.wall.getHeight() * j, null);
+				}
+////////////////////////////////////////OBJECTS/////////////////////////////////////////////////
+				/*if(this.map[j][i]=='k')
+				{
+					g.drawImage(this.leverClosed, this.wall.getWidth() * i, this.wall.getHeight() * j, null);
+				}*/
+				if(this.map[j][i]=='k')
+				{
+					g.drawImage(this.key, this.wall.getWidth() * i, this.wall.getHeight() * j, null);
 				}
 			}
 		}
