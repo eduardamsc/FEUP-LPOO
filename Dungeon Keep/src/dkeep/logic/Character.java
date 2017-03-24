6784f707255688d3cde659f013cff1786032396a
eduardamsc <mariaeduardascunha97@gmail.com>
2017-03-24 21:46:18 +0000

    reduçao de duplicaçao de codigo em pela criaçao de element

--- Dungeon Keep/src/dkeep/logic/Character.java
+++ Dungeon Keep/src/dkeep/logic/Character.java
@@ -2,60 +2,7 @@ package dkeep.logic;
 
 import java.util.Random;
 
-public abstract class Character {
-
-	protected int x; // lines
-	protected int y; // columns
-	protected char symbol; // represents character on map
-	
-	/**
-	 * @brief Constructor for class Character.
-	 */
-	public Character() {
-		x = 0;
-		y = 0;
-	}
-
-	/////////////////////////////////////////GETS AND SETS//////////////////////////////////////
-	/**
-	 * @brief Get for attribute X, which represents the lines.
-	 * @return x.
-	 */
-	public int getX() {
-		return x;
-	}
-	/**
-	 * @brief Get for attribute Y, which represents the columns.
-	 * @return y.
-	 */
-	public int getY() {
-		return y;
-	}
-	/**
-	 * @brief Set for attribute X.
-	 */
-	public void setX(int x) {
-		this.x = x;
-	}
-	/**
-	 * @brief Set for attribute Y.
-	 */
-	public void setY(int y) {
-		this.y = y;
-	}
-	/**
-	 * @brief Get for attribute symbol, which distinguishes each Character in the map.
-	 * @return symbol.
-	 */
-	public char getSymbol() {
-		return symbol;
-	}
-	/**
-	 * @brief Set for attribute symbol.
-	 */
-	public void setSymbol(char symbol) {
-		this.symbol = symbol;
-	}
+public abstract class Character extends Element{
 
 	/////////////////////////////////////////MOVEMENTS//////////////////////////////////////
 	/**
