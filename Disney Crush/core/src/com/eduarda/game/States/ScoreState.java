a966e92356e7c8bf6092d07de838b1a432ceaab6
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2017-06-10 12:48:14 +0100

    botão back score state

--- Disney Crush/core/src/com/eduarda/game/States/ScoreState.java
+++ Disney Crush/core/src/com/eduarda/game/States/ScoreState.java
@@ -4,6 +4,18 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+//@@ -10,6 +13,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.eduarda.game.DisneyCrushDemo;
 
 /**
  * Created by beatrizHm on 02/06/17.
@@ -14,12 +26,13 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
  */
 public class ScoreState extends State{
     private Texture background;
-    private Texture back;
+    private Texture button;
+    private Image back;
+    private Stage stage;
+    private Viewport vp;
 
     private String villains;
     private String princesses;
-
-    public float WIDTH = (float) ((((float) 8/10) * Gdx.graphics.getWidth())/8);
 
     /**
      * Constructor for ScoreState.
@@ -29,8 +42,37 @@ public class ScoreState extends State{
     public ScoreState(GameStateManager game){
         super(game);
 
+        vp = new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        vp.apply();
+
+        stage = new Stage(vp);
+
+        float hButton = (float) ((((float) 1f)*Gdx.graphics.getHeight())/8);
+        float wButton = (float) ((((float) 4/3)*Gdx.graphics.getWidth())/8);
+
+        float xButton = 0.5f; //Gdx.graphics.getWidth();
+        float yButton = 0.5f; //Gdx.graphics.getHeight();
+
         background = new Texture("scoreBackground.png");
-        back = new Texture("back.png");
+        button = new Texture("back.png");
+        back = new Image(button);
+        back.setHeight(hButton);
+        back.setWidth(wButton);
+        back.setPosition(xButton, yButton);
+
+        Gdx.input.setInputProcessor(stage);
+        stage.addActor(back);
+
+        back.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y){
+                setMenuState();
+            }
+        });
+    }
+
+    public void setMenuState(){
+        game.set(new MenuState(game));
     }
 
     /**
@@ -38,13 +80,6 @@ public class ScoreState extends State{
      */
     @Override
     protected void handleInput() {
-        if (Gdx.input.justTouched()) {
-            float realY = Gdx.graphics.getHeight() - Gdx.input.getY();
-            if (Gdx.input.getX()<2*WIDTH && realY<WIDTH) {
-                game.set(new MenuState(game));
-                dispose();
-            }
-        }
     }
 
     /**
@@ -53,7 +88,6 @@ public class ScoreState extends State{
     @Override
     public void update(float dt) {
         handleInput();
-
     }
 
     /**
@@ -61,12 +95,10 @@ public class ScoreState extends State{
      */
     @Override
     public void render(SpriteBatch sb) {
-        
         BitmapFont b = new BitmapFont(Gdx.files.internal("font.fnt"));
         b.setColor(1, 1, 1, 1);
         b.getData().scale(1);
-        
-        
+
         //villains
         villains = String.valueOf(PlayState.getScore());
         
@@ -75,11 +107,11 @@ public class ScoreState extends State{
         
         sb.begin();
             sb.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-            sb.draw(back, 0, 0, 2*WIDTH, WIDTH);
-        float factor = (float) ((6.7 - (villains.length() - 1) * 0.25) / 10.0);
-        b.draw(sb, villains, (float) factor*Gdx.graphics.getWidth(), (float) 7/12*Gdx.graphics.getHeight());
-        b.draw(sb, princesses, (float) factor*Gdx.graphics.getWidth(), (float) 6.10/12*Gdx.graphics.getHeight());
+            float factor = (float) ((6.7f-(villains.length() - 1)*0.25f)/10.0f);
+            b.draw(sb, villains, (float) factor*Gdx.graphics.getWidth(), (float) 7/12*Gdx.graphics.getHeight());
+            b.draw(sb, princesses, (float) factor*Gdx.graphics.getWidth(), (float) 6.10/12*Gdx.graphics.getHeight());
         sb.end();
+        stage.draw();
     }
 
     /**
@@ -87,7 +119,5 @@ public class ScoreState extends State{
      */
     @Override
     public void dispose() {
-        background.dispose();
-        back.dispose();
     }
 }
